f4751eb1bc8408b402f4d24e1080c2cbb5fa5d6f
Pamela Prybylski <mori@inari.garbagefile.net>
2021-03-22 11:39:16 -0400

    created populate function for table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select table body
+var tbody = d3.select('tbody');
+
+// Create function to populate the table
+function populate() {
+    tableData.map(data => {
+
+        // Make new row
+        var row = tbody.append('tr');
+
+        // append each row with data
+        row.append('td').text(data.datetime);
+        row.append('td').text(data.city);
+        row.append('td').text(data.state);
+        row.append('td').text(data.country);
+        row.append('td').text(data.shape);
+        row.append('td').text(data.durationMinutes);
+        row.append('td').text(data.comments);
+    });
+}
